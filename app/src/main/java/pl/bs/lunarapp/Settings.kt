f927093aa58abbfc8738078680e3917a6cfb3fdf
TheTerabit <terrabitroman@gmail.com>
2020-04-05 00:02:41 +0200

    add views

--- app/src/main/java/pl/bs/lunarapp/Settings.kt
+++ app/src/main/java/pl/bs/lunarapp/Settings.kt
@@ -3,12 +3,10 @@ package pl.bs.lunarapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
-
+class Settings : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_settings)
     }
 }
